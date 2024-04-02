511d1ef670351be6b2b68415d2051bd33878bc63
navin <navin5556@gmail.com>
2024-04-02 17:58:31 +0530

    implemented circuit-breaker in user-service

--- UserService/src/main/java/com/navin/user/service/controllers/UserController.java
+++ UserService/src/main/java/com/navin/user/service/controllers/UserController.java
@@ -3,6 +3,10 @@ package com.navin.user.service.controllers;
 
 import com.navin.user.service.entities.User;
 import com.navin.user.service.services.UserService;
+import com.navin.user.service.services.impl.UserServiceImpl;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +21,8 @@ public class UserController
     @Autowired
     private UserService userService;
 
+    private Logger logger = LoggerFactory.getLogger(UserController.class);
+
     //create
     @PostMapping
     public ResponseEntity<User> createUser(@RequestBody User user) {
@@ -26,11 +32,24 @@ public class UserController
 
     //single user get
     @GetMapping("/{userId}")
+    @CircuitBreaker(name = "ratingHotelBreaker", fallbackMethod = "ratingHotelFallback")
     public ResponseEntity<User> getSingleUser(@PathVariable String userId){
+        logger.info("Get Single User Handler: UserController");
         User user = userService.getUser(userId);
         return ResponseEntity.ok(user);
     }
 
+    //creating fallback method for circuit breaker
+    public ResponseEntity<User> ratingHotelFallback(String userId, Exception ex){
+        logger.info("Fallback is executed because service is down: ", ex.getMessage());
+        User user = User.builder()
+                .email("dummy@gmail.com")
+                .name("Dummy")
+                .about("This user is created dummy because some service is down")
+                .userId(userId)
+                .build();
+        return ResponseEntity.ok(user);
+    }
 
     //all user get
     @GetMapping
